90f18630d8b579667640370cd7f7a4caf04b3954
Brian Russom <brian.russom@gmail.com>
2019-10-09 17:12:51 +0000

    testing SSH

--- MotorControl.py
+++ MotorControl.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import RPi.GPIO as GPIO, time
 
 class StepperController:
+    
     coil_A_1_pin = 0
     coil_A_2_pin = 0
     coil_B_1_pin = 0
@@ -62,72 +62,7 @@ class StepperController:
                 StepCounter = 0
             if StepCounter < 0:
                 StepCounter = StepCount + StepDir
-=======
-import RPi.GPIO as GPIO, time
-
-class StepperController:
-    coil_A_1_pin = 0
-    coil_A_2_pin = 0
-    coil_B_1_pin = 0
-    coil_B_2_pin = 0
-
-    def __init__(self, pin1, pin2, pin3, pin4):
-        self.coil_A_1_pin = pin1
-        self.coil_A_2_pin = pin2
-        self.coil_B_1_pin = pin3
-        self.coil_B_2_pin = pin4
-        print self.coil_A_1_pin
-        print self.coil_A_2_pin
-        print self.coil_B_1_pin
-        print self.coil_B_2_pin
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.coil_A_1_pin, GPIO.OUT)
-        GPIO.setup(self.coil_A_2_pin, GPIO.OUT)
-        GPIO.setup(self.coil_B_1_pin, GPIO.OUT)
-        GPIO.setup(self.coil_B_2_pin, GPIO.OUT)
-        print 'pins set to output'
-        print 'Pins Set'
-
-    def setStep(self, w1, w2, w3, w4):
-        GPIO.output(self.coil_A_1_pin, w1)
-        GPIO.output(self.coil_A_2_pin, w2)
-        GPIO.output(self.coil_B_1_pin, w3)
-        GPIO.output(self.coil_B_2_pin, w4)
-
-    def TurnMotor(self, direction, steps):
-        StepPins = [
-         self.coil_A_1_pin, self.coil_A_2_pin, self.coil_B_1_pin, self.coil_B_2_pin]
-        Seq = [[1, 0, 0, 1],
-         [
-          1, 0, 0, 0],
-         [
-          1, 1, 0, 0],
-         [
-          0, 1, 0, 0],
-         [
-          0, 1, 1, 0],
-         [
-          0, 0, 1, 0],
-         [
-          0, 0, 1, 1],
-         [
-          0, 0, 0, 1]]
-        StepCount = len(Seq)
-        StepDir = direction
-        WaitTime = 5 / float(1000)
-        StepCounter = 0
-        for i in range(0, steps):
-            for pin in range(0, 4):
-                xpin = StepPins[pin]
-                if Seq[StepCounter][pin] != 0:
-                    GPIO.output(xpin, True)
-                else:
-                    GPIO.output(xpin, False)
-
-            StepCounter += StepDir
-            if StepCounter >= StepCount:
-                StepCounter = 0
-            if StepCounter < 0:
-                StepCounter = StepCount + StepDir
->>>>>>> 16e93d9beb5cb02bc019b91a7d5777294b6ab9e8
             time.sleep(WaitTime)
+
+class ServoController:
+    print ("hi")
--- MotorPlayground.py
+++ MotorPlayground.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import MotorControl
 import RPi.GPIO as GPIO
 from adafruit_servokit import ServoKit
@@ -25,29 +25,5 @@ except KeyboardInterrupt:
   GPIO.output(22, False)
   GPIO.output(24, False)
   GPIO.output(4, False)
-=======
-import MotorControl
-import RPi.GPIO as GPIO
 
-currentPos = 0
-DesiredPosition =0
-
-sorter = MotorControl.StepperController(20,22,24,4)
-sorter.setStep(0,0,0,0)
-print ("pins freed")
-numSteps = 512
-speed = 2
-Direction = speed *-1
-
-try:
-  while True:
-      sorter.TurnMotor(speed, numSteps)
-        #print ("moved back")
-except KeyboardInterrupt:
-  
-  GPIO.output(20, False)
-  GPIO.output(22, False)
-  GPIO.output(24, False)
-  GPIO.output(4, False)
->>>>>>> 16e93d9beb5cb02bc019b91a7d5777294b6ab9e8
    
